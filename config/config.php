7bb7cce9a77483c2daeb072191847fc00f778bca
nourane-hammouda <160539719+nourane-hammouda@users.noreply.github.com>
2025-03-05 11:41:18 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+// Inclure le fichier contenant les identifiants
+require_once __DIR__ . '/config.inc.php';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USER,
+        DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
